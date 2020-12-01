e597dd4611ae61293d1f66ecd43413a5eb8ed990
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-01 19:09:56 +0000

    moving

--- client.js
+++ client.js
@@ -13,6 +13,11 @@ const connect = function() {
   conn.on("connect", () => console.log("Successfully connected"));
   conn.on("data", (data) => console.log("Server says : ", data));
   conn.on('connect', () => conn.write('Name: KEL'));
+  /*
+  conn.on('connect', () => conn.write('Move: up'));
+  conn.on('connect', () => setTimeout(function() {conn.write('Move: left');}, 500));
+  conn.on('connect', () => setTimeout(function() {conn.write('Move: up');}, 1000));
+  conn.on('connect', () => setTimeout(function() {conn.write('Move: right');}, 1500));*/
   return conn;
 
 }
