f5f244a144b0ee0e1db24c337cc9dc4f73052c56
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-02 14:06:45 +0000

    add constant

--- client.js
+++ client.js
@@ -1,18 +1,20 @@
 const net = require('net');
+const {IP, PORT} = require('./constants')
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on("connect", () => console.log("Successfully connected"));
   conn.on("data", (data) => console.log("Server says : ", data));
   conn.on('connect', () => conn.write('Name: KEL'));
+  conn.on('connect', () => conn.write('Say: Go on'));
   return conn;
 
 }
