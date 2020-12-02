ffd9e8c1a373f8e60bf489ae459844981a4ddaec
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-02 00:35:13 +0000

    add input file

---
+++ input.js
@@ -0,0 +1,14 @@
+const handleUserInput = function(k) {
+  if (k === '\u0003') {
+  process.exit();
+  }
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+}
+module.exports = {setupInput};
